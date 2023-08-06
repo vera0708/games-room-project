a194a385c0a5e1b6693755be0127315453c04a75
vera0708 <verasdee@gmail.com>
2023-08-06 22:20:58 +0700

    feat: made accordion and media for modal

---
+++ script/accordion.js
@@ -0,0 +1,26 @@
+const items = document.querySelectorAll('.faq__item');
+const buttons = document.querySelectorAll('.faq__btn');
+const textWrapper = document.querySelectorAll('.faq__text-wrapper')
+
+let heightWrapper = 0;
+textWrapper.forEach(elem => {
+    if (heightWrapper < elem.scrollHeight) {
+        heightWrapper = elem.scrollHeight;
+    }
+});
+
+buttons.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        for (let i = 0; i < items.length; i++) {
+            if (index === i) {
+                textWrapper[i].style.height =
+                    items[i].classList.contains('faq__item_active')
+                        ? '' : `${heightWrapper}px`;
+                items[i].classList.toggle('faq__item_active');
+            } else {
+                items[i].classList.remove('faq__item_active');
+                textWrapper[i].style.height = '';
+            }
+        }
+    });
+});
