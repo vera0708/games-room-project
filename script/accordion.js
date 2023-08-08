a4df444bdc8c89c4c6f3202df70de3ba99cc882b
vera0708 <verasdee@gmail.com>
2023-08-08 11:19:45 +0700

    feat: made + / - for accordion

--- script/accordion.js
+++ script/accordion.js
@@ -17,8 +17,16 @@ buttons.forEach((btn, index) => {
                     items[i].classList.contains('faq__item_active')
                         ? '' : `${heightWrapper}px`;
                 items[i].classList.toggle('faq__item_active');
+                items[i].querySelector('.faq__btn').classList.toggle('faq__btn_active');
+
+                if (i % 2 === 0) {
+                    console.log(i, 'чётное');
+                } else {
+                    console.log(i, 'нечётное');
+                }
             } else {
                 items[i].classList.remove('faq__item_active');
+                items[i].querySelector('.faq__btn').classList.toggle('active');
                 textWrapper[i].style.height = '';
             }
         }
