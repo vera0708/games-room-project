a95eae0698c2a933ab0949f00bb2ca5cb36627d7
Vera Deeva <verasdee@gmail.com>
2023-11-01 21:14:38 +0700

    feat: add swiper and yamap plagins amd make changes accordently

---
+++ script/script.js
@@ -0,0 +1,7 @@
+ymaps.ready(init);
+function init() {
+    const goMap = new ymaps.Map("map", {
+        center: [55.740036, 37.637416],
+        zoom: 17
+    });
+}
---
+++ script/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    loop: true,
+    mousewheel: true,
+    keyboard: true,
+    // breakpoints: {
+    //     580: {
+    //         direction: 'vertical'
+    //     }
+    // }
+});
