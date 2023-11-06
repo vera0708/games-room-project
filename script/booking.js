f44398fcdfd214ec8bd55fb372d2497b2b4f1ca1
Vera Deeva <verasdee@gmail.com>
2023-11-06 12:45:44 +0700

    feat: add time Picker and update JustValidate for and booking forms

--- script/booking.js
+++ script/booking.js
@@ -1,15 +1,79 @@
-const inputTel = document.querySelector('.person-info__tel');
+const validateBooking = (bookingForm) => {
 
-const telMask = new Inputmask('+7 (999)-999-99-99');
-telMask.mask(inputTel);
+    // console.log('bookingForm: ', bookingForm);
+    const inputTel = document.querySelector('.person-info__tel');
+    const telMask = new Inputmask('+7 (999)-999-99-99');
+    telMask.mask(inputTel);
 
-const justValidate = new JustValidate('.booking__form');
+    flatpickr(".person-info__time", {
+        enableTime: true,
+        noCalendar: true,
+        dateFormat: "H:i",
+        maxTime: "22:30",
+        time_24hr: true
+    });
 
-justValidate
-    .addField('.person-info__name', [
-        {
+    const justValidate = new JustValidate(bookingForm, {
+        errorLabelStyle: {
+            color: '#f00',
+        },
+        errorFieldCssClass: 'invalid',
+        errorsContainer: document.querySelector('.booking__error'),
+        // tooltip: {
+        //     position: 'bottom',
+        // },
+    });
+
+    justValidate
+        .addRequiredGroup('.field-halls', 'Выберите зал')
+        .addField('.person-info__date', [{
             rule: 'required',
-            errorMessage: 'Укажите ваше имя',
+            errorMessage: 'Укажите дату',
+        },
+        {
+            plugin: JustValidatePluginDate(() => ({
+                format: 'yyyy-MM-dd',
+            })),
+            errorMessage:
+                'Укажите дату в формате dd MM yyyy',
+        },
+        ])
+
+        .addField('.person-info__time', [{
+            rule: 'required',
+            errorMessage: 'Укажите время',
+        }])
+        .addField('.person-info__people', [{
+            rule: 'number',
+            errorMessage: 'Укажите количество посетителей',
+        },
+        {
+            rule: 'minNumber',
+            value: 1,
+        },
+        {
+            rule: 'maxNumber',
+            value: 15,
+        },])
+        .addField('.person-info__name', [
+            {
+                rule: 'required',
+                errorMessage: 'Укажите ваше имя',
+            },
+            {
+                rule: 'minLength',
+                value: 2,
+                errorMessage: 'Не короче 2-х символов',
+            },
+            {
+                rule: 'maxLength',
+                value: 18,
+                errorMessage: 'Не длинее 18-и символов',
+            },
+        ])
+        .addField('.person-info__surname', [{
+            rule: 'required',
+            errorMessage: 'Укажите вашу фамилию',
         },
         {
             rule: 'minLength',
@@ -18,51 +82,68 @@ justValidate
         },
         {
             rule: 'maxLength',
-            value: 18,
-            errorMessage: 'Не длинее 18-и символов',
-        },
-    ])
-    .addField('.person-info__email', [
-        {
-            rule: 'required',
-            errorMessage: 'Укажите ваш email',
-        },
-        {
-            rule: 'email',
-            errorMessage: 'email не корректный!',
-        },
-    ])
-    .addField('.person-info__tel', [
-        {
-            rule: 'required',
-            errorMessage: 'Укажите ваш телефон',
-        },
-        {
-            validator(value) {
-                const phone = inputTel.inputmask.unmaskedvalue();
-                return !!(Number(phone) && phone.length === 10);
+            value: 30,
+            errorMessage: 'Не длинее 30-и символов',
+        },])
+        .addField('.person-info__email', [
+            {
+                rule: 'required',
+                errorMessage: 'Укажите ваш email ',
             },
-            errorMessage: 'Телефон не корректный!'
-        },
-    ])
-    .onSuccess(eve => {
-        const target = eve.target;
-        axios.post('https://jsonplaceholder.typicode.com/posts', {
-            firstName: target.firstName.value,
-            lastName: target.lastName.value,
-            phone: inputTel.inputmask.unmaskedvalue(),
-            email: target.email.value,
+            {
+                rule: 'email',
+                errorMessage: 'email не корректный!',
+            },
+        ])
+        .addField('.person-info__tel', [
+            {
+                rule: 'required',
+                errorMessage: 'Укажите ваш телефон',
+            },
+            {
+                validator(value) {
+                    const phone = inputTel.inputmask.unmaskedvalue();
+                    return !!(Number(phone) && phone.length === 10);
+                },
+                errorMessage: 'Телефон не корректный!'
+            },
+        ])
+        .onSuccess(eve => {
+            const target = eve.target;
+            axios.post('https://jsonplaceholder.typicode.com/posts', {
+                firstName: target.firstName.value,
+                lastName: target.lastName.value,
+                phone: inputTel.inputmask.unmaskedvalue(),
+                email: target.email.value,
 
-        })
-            .then(response => {
-                console.log('response: ', response);
-                target.reset();
-                // modalOrderFielfset.disabled = true;
-                // modalOrderTitle.textContent = `Спасибо, ваша заявка принята. Номер заявки ${response.data.id}`
             })
-            .catch(err => {
-                console.error(err);
-                // modalOrderFielfset.disabled = false;
-                // modalOrderTitle.textContent = `Что-то пошло не так, попробуйте позже`
-            });
-    });
+                .then(response => {
+                    console.log('response: ', response);
+                    target.reset();
+                    // modalOrderFielfset.disabled = true;
+                    // modalOrderTitle.textContent = `Спасибо, ваша заявка принята. Номер заявки ${response.data.id}`
+                })
+                .catch(err => {
+                    console.error(err);
+                    // modalOrderFielfset.disabled = false;
+                    // modalOrderTitle.textContent = `Что-то пошло не так, попробуйте позже`
+                });
+        });
+    return justValidate;
+};
+
+const bookingControler = () => {
+    const bookingForm = document.querySelector('.booking__form');
+    const validate = validateBooking(bookingForm);
+
+    bookingForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        if (validate.isValid) {
+            console.log('Отправка формы')
+        }
+    })
+};
+
+bookingControler();
+
--- script/modal.js
+++ script/modal.js
@@ -33,74 +33,79 @@ const modalControl = () => {
 };
 const { closeModal } = modalControl();
 
-const formControl = (closeModal) => {
-    const form = document.querySelector('.modal__form');
-
-    form.addEventListener('submit', e => {
-        e.preventDefault();
-        const formData = new FormData(e.target);
-        const newCall = Object.fromEntries(formData);
-        console.log(newCall);
-        form.reset();
-        closeModal();
-    })
-};
-formControl(closeModal);
-
 const modalTitle = document.querySelector('.modal__title');
 
 const inputTel = document.querySelector('.form__input-tel');
-
 const telMask = new Inputmask('+7 (999)-999-99-99');
 telMask.mask(inputTel);
 
-const justValidate = new JustValidate('."modal__form');
-
-justValidate
-    .addField('.form__input-name', [
-        {
-            rule: 'required',
-            errorMessage: 'Укажите ваше имя',
+const validationModal = (form) => {
+    const justValidate = new JustValidate(form, {
+        errorLabelStyle: {
+            color: '#f00',
         },
-        {
-            rule: 'minLength',
-            value: 2,
-            errorMessage: 'Не короче 2-х символов',
-        },
-        {
-            rule: 'maxLength',
-            value: 18,
-            errorMessage: 'Не длинее 18-и символов',
-        },
-    ])
-    .addField('.form__input-tel', [
-        {
-            rule: 'required',
-            errorMessage: 'Укажите ваш телефон',
-        },
-        {
-            validator(value) {
-                const phone = inputTel.inputmask.unmaskedvalue();
-                return !!(Number(phone) && phone.length === 10);
-            },
-            errorMessage: 'Телефон не корректный!'
-        },
-    ])
-    .onSuccess(eve => {
-        const target = eve.target;
-        axios.post('https://jsonplaceholder.typicode.com/posts', {
-            modalName: target.modalName.value,
-            modalPhone: inputTel.inputmask.unmaskedvalue(),
-        })
-            .then(response => {
-                console.log('response: ', response);
-                target.reset();
-                // modalOrderFielfset.disabled = true;
-                modalTitle.textContent = `Спасибо, ваша заявка принята. Номер заявки ${response.data.id}`
-            })
-            .catch(err => {
-                console.error(err);
-                // modalOrderFielfset.disabled = false;
-                modalTitle.textContent = `Что-то пошло не так, попробуйте позже`
-            });
     });
+
+    justValidate
+        .addField('.form__input-name', [
+            {
+                rule: 'required',
+                errorMessage: 'Укажите ваше имя',
+            },
+            {
+                rule: 'minLength',
+                value: 2,
+                errorMessage: 'Не короче 2-х символов',
+            },
+            {
+                rule: 'maxLength',
+                value: 18,
+                errorMessage: 'Не длинее 18-и символов',
+            },
+        ])
+        .addField('.form__input-tel', [
+            {
+                rule: 'required',
+                errorMessage: 'Укажите ваш телефон',
+            },
+            {
+                validator(value) {
+                    const phone = inputTel.inputmask.unmaskedvalue();
+                    return !!(Number(phone) && phone.length === 10);
+                },
+                errorMessage: 'Телефон не корректный!'
+            },
+        ])
+        .onSuccess(eve => {
+            const target = eve.target;
+            axios.post('https://jsonplaceholder.typicode.com/posts', {
+                modalName: target.modalName.value,
+                modalPhone: inputTel.inputmask.unmaskedvalue(),
+            })
+                .then(response => {
+                    console.log('response: ', response);
+                    target.reset();
+                    // modalOrderFielfset.disabled = true;
+                    modalTitle.textContent = `Спасибо, ваша заявка принята. Номер заявки ${response.data.id}`
+                })
+                .catch(err => {
+                    console.error(err);
+                    // modalOrderFielfset.disabled = false;
+                    modalTitle.textContent = `Что-то пошло не так, попробуйте позже`
+                });
+        });
+};
+
+const formControl = (closeModal) => {
+    const form = document.querySelector('.modal__form');
+    validationModal(form);
+
+    form.addEventListener('submit', e => {
+        e.preventDefault();
+        // const formData = new FormData(e.target);
+        // const newCall = Object.fromEntries(formData);
+        // form.reset();
+        // closeModal();
+    })
+};
+formControl(closeModal);
